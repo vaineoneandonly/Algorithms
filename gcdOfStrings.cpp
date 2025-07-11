afa0e00f46b739588404a61bd9ef785c4de00360
Vinícius Dias da Rocha <35822025+vaineoneandonly@users.noreply.github.com>
2025-07-11 01:15:19 -0300

    LC75 GCD exercise, with own GCD and substring func

--- gcdOfStrings.cpp
+++ gcdOfStrings.cpp
@@ -1,57 +1,44 @@
 import std;
 
-std::string gcdOfStrings(std::string str1, std::string str2)
+int euclideanGCD(int a, int b)
 {
-	std::string res;
-
-	if (str1 + str2 != str2 + str1) res = "";
-	else
-	{
-		bool firstBigger{ (str1.size() > str2.size()) ? true : false };
-
-		char *p{ (firstBigger) ? &(str1[str2.size()]) : &(str2[str1.size()]) };
-
-		while (*p != '\0')
-		{
-			res += *p;
-			++p;
-		}
-	}
-
-	return res;
+	if		(a == 0) return b;
+	else if (b == 0) return a;
+	else return euclideanGCD(b, a % b);
 }
 
-int main()
+std::string mySubstr(std::string s, int offset, int max)
+{ 
+	std::string ns;
+	char *p{ &(s[offset]) };
+
+	for (int i = 0; i < max; ++i)
+	{
+		ns += *p;
+		++p;
+	}
+	return ns;
+}
+
+std::string strGCD(std::string s1, std::string s2)
 {
-	std::string s{"ABC"};
-	std::string t{"ABCABC"};
-	std::string res;
-
-	//bool secondBigger{ (t.size() > s.size()) ? true : false };
-	bool firstBigger{ (s.size() > t.size()) ? true : false };
-
-	char *p{&(s[0])};
-
-	char *p2{ (firstBigger) ? &(s[0]) : &(t[0]) };
-
-	if (s + t != t + s) res = "";
+	if (s1 + s2 != s2 + s1) return "";
 	else
 	{
-		//p += (secondBigger) ? t.size() : s.size();
-		p2 += (firstBigger) ? t.size() : s.size();
+		int l1{ static_cast<int>(s1.size()) };
+		int l2{ static_cast<int>(s2.size()) };
 
-		while (*p2 != '\0')
-		{ 
-			res += *p2;
-			++p2;
-		}
-
+		//int lGCD{ std::gcd(l1, l2) };
+		int lGCD{ euclideanGCD(l1, l2) };
+		
+		
+		//return s1.substr(0, lGCD);
+		return mySubstr(s1, 0, lGCD);
 	}
-
-	std::cout << '\n' << res << '\n';
-
+}
 
 
-	std::cout << gcdOfStrings("ABCABCABC", "ABC");
-
+int main()
+{	
+	std::cout << strGCD("ABABAB", "ABAB");
 }
