baf20ea2eea1fa220f5033bdf9c59a929f0d8c38
Vinícius Dias da Rocha <35822025+vaineoneandonly@users.noreply.github.com>
2025-07-11 20:57:51 -0300

    very proud of this one lol.

--- flowers.cpp
+++ flowers.cpp
@@ -1,37 +1,30 @@
 import std;
 
-int main()
+bool canPlaceFlowers(std::vector<int> &flowerbed, int n)
 {
-	std::vector<int> f1{ 1, 0, 0, 0, 1 };
-	int newFlowers{ 0 };
+	int	  nf{ 0 };
 	int dump{ 0 };
 
-	int *mp{ &(f1[0]) };
-
-	int *lp{ (mp == &(f1.front())) ? &dump : mp - 1 };
-	int *rp{ (mp == &(f1.back())) ? &dump : mp + 1 };
-
-	std::cout << lp << '\n'
-		<< mp << '\n'
-		<< rp << '\n';
-
-	for (int i = 0; i < f1.size(); ++i)
+	for (int i = 0; i < flowerbed.size(); ++i)
 	{
-		for (int f : f1) std::cout << f;
-		std::cout << '\n';
-
-		mp = &(f1[i]);
-
-
-		lp = (mp == &(f1.front())) ? &dump : mp - 1;
-		rp = (mp == &(f1.back())) ? &dump : mp + 1;
-
-		if ((*mp + *lp + *rp) == 0)
+		int *mp{ &flowerbed[i] };
+		int *lp{ mp == &flowerbed.front() ? &dump : mp - 1 };
+		int *rp{ mp == &flowerbed.back()  ? &dump : mp + 1 };
+	
+		if ((*lp + *mp + *rp) == 0)
 		{
 			*mp = 1;
-			newFlowers += 1;
+			++nf;
 		}
 	}
 
-	std::cout << newFlowers;
+	return n <= nf;
+}
+
+int main()
+{
+	std::vector<int> f1{ 1, 0, 0, 0, 1 };
+	std::cout << canPlaceFlowers(f1, 3);
+
+
 }
